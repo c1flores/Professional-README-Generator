3373106e28612eafc90df0ff99b4b236851cde35
Christian Flores <c1flores@ucsd.edu>
2022-10-02 18:05:58 -0700

    Added render functions as well as markdown generator functions

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -1,18 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![Github License](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ""
+}
 
-// TODO: Create a function that returns the license section of README
+
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `\n* [License](#license)\n`
+    )
+  }
+  return ""
+}
 
-// TODO: Create a function to generate markdown for README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License 📛 
+
+      Copyright @ ${license}. All rights reserved.
+
+      Licensed under the ${license} license.`
+    )
+  }
+  return ""
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## GitHub URL 🌐
+
+  [${data.github}](https://github.com/${data.github}/)
+
+  ## Description 📝
+
+  ${data.description}
+
+  ## Table of Contents 🗒
+
+  * [Installations](#dependencies)
+
+  * [Usage](#usage)
+
+  ${renderLicenseLink(data.license)}
+
+  * [Contributors](#contributors)
+
+  * [Test](#test)
+
+  ## Installations (Dependencies) 💻
+
+  To install dependencies, run these commands:
+
+  \`\`\`
+  ${data.dependencies}
+  \`\`\`
+
+  ## Usage 🏆
+
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributors 😃
+
+  ${data.contributors}
+
+  Contact me at ${data.email}
+
+  ## Tests 🧪
+
+  To run tests, run these commands:
+
+  \`\`\`
+  $(data.test)
+  \`\`\`
 
 `;
 }
